3ec7389e87fe58ee923d0ae7a71028675a8de477
UserNanno <juan.canecillas@unmsm.edu.pe>
2024-12-02 00:38:39 -0500

    .......

--- Dynamus/wsgi.py
+++ Dynamus/wsgi.py
@@ -2,6 +2,15 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django.core.management import call_command
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dynamus.settings')
 
 application = get_wsgi_application()
+
+# Ejecutar migraciones automáticamente en el inicio de la aplicación
+try:
+    call_command('migrate', interactive=False)
+    print("Migraciones aplicadas correctamente.")
+except Exception as e:
+    print(f"Error al aplicar migraciones: {e}")
